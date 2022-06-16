617209e194f8e9eea2b31969d10a64c26b12037b
FeiFeiSeal <99242308+FeiFeiSeal@users.noreply.github.com>
2022-06-16 21:39:17 +0800

    Update index.js

--- 20220613-REchatTalkerAI-RWD/index.js
+++ 20220613-REchatTalkerAI-RWD/index.js
@@ -1,9 +1,4 @@
-let chargeOri = []
 
-function getCharge(){
-  return axios.get('./data/chargeData.json')
-    .then((res)=> chargeOri = res.data)
-}
 function getHeader(){
   return fetch('./components/header.html').then((res)=> res.text())
 }
@@ -13,7 +8,6 @@ function getCopyRight(){
 
 console.log("%c內容純屬虛構", "color:red; font-size:40px");
 
-getCharge();
 window.onload = function(){
   const App = document.getElementById("app");
   const mainFooter = document.querySelector(".main-footer");
@@ -85,7 +79,7 @@ window.onload = function(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
-  // sweetAlertBtn();
+  // sweetAlertBtn設定;
   function sweetAlertBtn(){
     let setExperBtn = document.querySelectorAll(".experience-btn");
     //監聽物件啟動 sweetAlert
@@ -102,117 +96,127 @@ window.onload = function(){
 
   /*-----charge資費表載入------------------------------*/
   //載入資費清單
-  let chargeArr = chargeOri.map((obj)=> obj.charge )
-  let chargeInit = '10000';
-  
-  //按鈕選取效果添加與移除
-  function isActive(btn){
-    if(!btn){ 
-      let initBtn = Array.from(chargeList.childNodes).filter((e)=> e.dataset.charge === chargeInit)
-      initBtn[0].classList.add('active');
-      return;
-    };
-    chargeList.childNodes.forEach((el)=> el.classList.remove('active'));
-    btn.classList.add('active');
-  }
-  //render清單
-  function renderchargeList(){
-    chargeList.innerHTML = chargeArr.map((el)=> `<li class="charge-level" data-charge="${el}"><span>${el}</span>位</li>`).join('');
-    isActive();
-  }
-  //render資料
-  function render(e){
-    let getCharge = '';
-    if( e.target === this) return;
-    if(e.target.matches('li')){
-      getCharge = e.target.dataset.charge;
-      isActive(e.target);
-    } 
-    if(e.target.matches('span')){
-      getCharge = e.target.parentNode.dataset.charge;
-      isActive(e.target.parentNode);
-    }
-    rederChargeBox(getCharge);
-  }
-  function rederChargeBox(getCharge){
-    let chargeBox = document.querySelector('.charge-box');
-    let getChargeObj = chargeOri.filter((obj)=> obj.charge === getCharge );
-    let basicCost = getChargeObj[0].project[0].cost;
-    let norCost = getChargeObj[0].project[1].cost;
+  async function renderchargeBlock(){
+    let getChargeData = await axios.get('./data/chargeData.json')
 
-    chargeBox.innerHTML = `
-        <div id="basicCharge" class="charge-card">
-            <h4 class="card-title">基本版</h4>
-            <div class="charge-detail">
-                <p>提供中小企業最必要的功能<br>
-                  完整建立分眾以達到精準的行銷自動化</p>
-                <div class="count">
-                    <div class="charge-choose">
-                        <h5>訂閱用戶數
-                            <div class="charge-item">
-                                <span class="person">${getCharge}</span>
-                                <span class="unit">位</span>
-                            </div>
-                        </h5>
-                    </div>
-                    <div class="charge-count">
-                        <h5>試算每月的費用
-                            <div class="charge-item">
-                                <span class="num">${basicCost}</span>
-                                <span class="unit">TWD</span>
-                            </div>
-                        </h5>
-                    </div>
-                </div>
-            </div>
-            <ul class="charge-serve">
-                <li>支援 Messenger / Instagram 或 LINE 渠道</li>
-                <li>10 個進階套件</li>
-                <li>可設定 50 種分眾標籤</li>
-                <li>客服人機轉接</li>
-            </ul>
-            <div class="btn-box">
-                <a href="#" class="btn experience-btn">立即體驗</a>
-            </div>
-        </div>
-        <div id="norCharge" class="charge-card">
-            <h4 class="card-title">標準版</h4>
-            <div class="charge-detail">
-                <p>建立企業流量池，全通路數據整合與交換<br>
-                    高可靠性服務</p>
-                <div class="count">
-                    <div class="charge-choose">
-                        <h5>訂閱用戶數
-                            <div class="charge-item">
-                                <span class="person">${getCharge}</span>
-                                <span class="unit">位</span>
-                            </div>
-                        </h5>
-                    </div>
-                    <div class="charge-count">
-                        <h5>試算每月的費用
-                            <div class="charge-item">
-                                <span class="num">${norCost}</span>
-                                <span class="unit">TWD</span>
-                            </div>
-                        </h5>
-                    </div>
-                </div>
-            </div>
-            <ul class="charge-serve">
-                <li>支援 Messenger / Instagram 或 LINE 渠道</li>
-                <li>20 個進階及行銷互動套件</li>
-                <li>可設定 500 種分眾標籤</li>
-                <li>客服人機轉接＋自動指派專員處理</li>
-            </ul>
-            <div class="btn-box">
-                <a href="#" class="btn experience-btn">立即體驗</a>
-            </div>
-        </div>`
-    
-    //重新渲染btn資料
-    sweetAlertBtn();
+    let chargeArr = getChargeData.data.map((obj)=> obj.charge )
+    let chargeInit = '10000';
+
+    renderchargeList();
+    rederChargeBox(chargeInit, getChargeData.data)
+    //render清單
+    //必須先接收到axios打回來的資料
+    function renderchargeList(){
+      chargeList.innerHTML = chargeArr.map((el)=> `<li class="charge-level" data-charge="${el}"><span>${el}</span>位</li>`).join('');
+      isActive();
+    }
+    //chargeList按鈕選取效果添加與移除
+    //必須先讓chargeList render 出來
+    function isActive(btn){
+      if(!btn){ 
+        let initBtn = Array.from(chargeList.childNodes).filter((e)=> e.dataset.charge === chargeInit)
+        initBtn[0].classList.add('active');
+        return;
+      };
+      chargeList.childNodes.forEach((el)=> el.classList.remove('active'));
+      btn.classList.add('active');
+    }
+
   }
+      // 按按鈕傳送 render 目標
+      function render(e){
+        let getCharge = '';
+        if( e.target === this) return;
+        if(e.target.matches('li')){
+          getCharge = e.target.dataset.charge;
+          isActive(e.target);
+        } 
+        if(e.target.matches('span')){
+          getCharge = e.target.parentNode.dataset.charge;
+          isActive(e.target.parentNode);
+        }
+        rederChargeBox(getCharge);
+      }
+      //接收 reder 目標
+      function rederChargeBox(getCharge, getChargeData){
+        let chargeBox = document.querySelector('.charge-box');
+            let  getChargeObj = getChargeData.filter((obj)=> obj.charge === getCharge )
+            let basicCost = getChargeObj[0].project[0].cost;
+            let norCost = getChargeObj[0].project[1].cost;
+  
+            chargeBox.innerHTML = `
+                <div id="basicCharge" class="charge-card">
+                    <h4 class="card-title">基本版</h4>
+                    <div class="charge-detail">
+                        <p>提供中小企業最必要的功能<br>
+                          完整建立分眾以達到精準的行銷自動化</p>
+                        <div class="count">
+                            <div class="charge-choose">
+                                <h5>訂閱用戶數
+                                    <div class="charge-item">
+                                        <span class="person">${getCharge}</span>
+                                        <span class="unit">位</span>
+                                    </div>
+                                </h5>
+                            </div>
+                            <div class="charge-count">
+                                <h5>試算每月的費用
+                                    <div class="charge-item">
+                                        <span class="num">${basicCost}</span>
+                                        <span class="unit">TWD</span>
+                                    </div>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="charge-serve">
+                        <li>支援 Messenger / Instagram 或 LINE 渠道</li>
+                        <li>10 個進階套件</li>
+                        <li>可設定 50 種分眾標籤</li>
+                        <li>客服人機轉接</li>
+                    </ul>
+                    <div class="btn-box">
+                        <a href="#" class="btn experience-btn">立即體驗</a>
+                    </div>
+                </div>
+                <div id="norCharge" class="charge-card">
+                    <h4 class="card-title">標準版</h4>
+                    <div class="charge-detail">
+                        <p>建立企業流量池，全通路數據整合與交換<br>
+                            高可靠性服務</p>
+                        <div class="count">
+                            <div class="charge-choose">
+                                <h5>訂閱用戶數
+                                    <div class="charge-item">
+                                        <span class="person">${getCharge}</span>
+                                        <span class="unit">位</span>
+                                    </div>
+                                </h5>
+                            </div>
+                            <div class="charge-count">
+                                <h5>試算每月的費用
+                                    <div class="charge-item">
+                                        <span class="num">${norCost}</span>
+                                        <span class="unit">TWD</span>
+                                    </div>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                    <ul class="charge-serve">
+                        <li>支援 Messenger / Instagram 或 LINE 渠道</li>
+                        <li>20 個進階及行銷互動套件</li>
+                        <li>可設定 500 種分眾標籤</li>
+                        <li>客服人機轉接＋自動指派專員處理</li>
+                    </ul>
+                    <div class="btn-box">
+                        <a href="#" class="btn experience-btn">立即體驗</a>
+                    </div>
+                </div>`
+        //重新渲染sweetalert
+        sweetAlertBtn();
+      }
+  
   
 
 
@@ -235,10 +239,10 @@ window.onload = function(){
 
   //如果有抓到 chargeList 存在才會觸發
   if(chargeList){
-    
     //charge 頁面預設呈現
-    rederChargeBox(chargeInit);
-    renderchargeList();
+    renderchargeBlock();
+    // rederChargeBox(chargeInit);
+    // renderchargeList();
     chargeList.addEventListener('click', render);
   }
   
